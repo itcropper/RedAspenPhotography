292e43356dd89d2bec448c26ddecb71b9329cb63
Ian <itcropper@gmail.com>
2015-03-06 08:58:30 -0700

    email now works

--- server/routeActions/contact.js
+++ server/routeActions/contact.js
@@ -2,22 +2,39 @@ var nodemailer = require('nodemailer'),
     bodyParser =    require('body-parser'),
     sendmail = require('sendmail')();
 
+var transporter = nodemailer.createTransport('SMTP', {
+    service: 'Gmail',
+    debug: true,
+    auth: {
+        user: 'thelittleshopgraphics@gmail.com',
+        pass: 'TeCz1313'
+    }
+});
 
 
 exports.email = function (req, res) {
-    
+    // create reusable transporter object using SMTP transport
 
-sendmail({
-    from: req.body.email,
-    to: 'itcropper@gmail.com',
-    subject: 'Message on RED ASPEN, From: ' + req.body.name,
-    content: req.body.message,
-  }, function(err, reply) {
-    if(err){
-        res.end('Sorry, Cannot send that email right now. Please Try again later');   
-    }else{
-        res.end("Email Sent! Thank you, I'll get back to you shortly.");
-    }
+
+    // NB! No need to recreate the transporter object. You can use
+    // the same transporter object for all e-mails
+
+    // setup e-mail data with unicode symbols
+    var mailOptions = {
+        from: req.body.name, // sender address
+        to: 'thelittleshopgraphics@gmail.com', // list of receivers
+        subject: 'RED ASPEN PHOTOGRAPHY', // Subject line
+        text: 'You have a message from: \n' + req.body.name + '\nat: \n ' + req.body.email + '\n\n' + req.body.message, // plaintext body
+        html: '' // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, function(error, info){
+        if(error){
+            res.end('Sorry, something went wrong and can\'t get your message. Try copying my email address into your email client.');
+        }else{
+            res.end('Thank you. I\'ll get back to you as quick as I can!');
+        }
 });
 }
 
