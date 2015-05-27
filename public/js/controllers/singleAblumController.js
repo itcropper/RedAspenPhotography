588606fa5f3a706f0f6207600e6af7c1f3d9d6a9
Ian <itcropper@gmail.com>
2015-05-27 00:01:50 -0600

    major redesign

--- public/js/controllers/singleAblumController.js
+++ public/js/controllers/singleAblumController.js
@@ -3,9 +3,30 @@ app.controller('singleAlbumController', [
     '$http',
     '$routeParams',
     function singleAlbumController($scope, $http, $routeParams){
+        
+    function shuffle(array) {
+          var currentIndex = array.length, temporaryValue, randomIndex ;
+
+          // While there remain elements to shuffle...
+          while (0 !== currentIndex) {
+
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+          }
+
+          return array;
+        }
+        
         $('.site-loader').show();
         $http.get('/js/data/albums.json').then(function(res){
             $scope.images = res.data[$routeParams.name.toLowerCase()].images;
+            $scope.images = shuffle($scope.images);
             $scope.title = res.data[$routeParams.name.toLowerCase()].title;
             
             setTimeout(function(e){
